70d1d956b038ae792100efc3afa878ee20e99b5c
MadMarchello228 <mark1010_00@mail.ru>
2019-04-29 01:26:27 +0700

    Create main window

--- neyron_base_data_creator/neyron_base_data_creator/main.cpp
+++ neyron_base_data_creator/neyron_base_data_creator/main.cpp
@@ -6,5 +6,21 @@ using namespace sf;
 
 int main()
 {
+	//создание главного окна приложения
+	RenderWindow window(VideoMode(1200, 720), "NeyronBaseDataCreator");
 
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
