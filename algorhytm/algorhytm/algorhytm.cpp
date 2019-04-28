34a235ad80967ef63173c9739c67fdb90ace30a9
Артём Гребцов <he-l-l-o@mail.ru>
2019-04-28 22:37:13 +0700

    rename variables

--- algorhytm/algorhytm/algorhytm.cpp
+++ algorhytm/algorhytm/algorhytm.cpp
@@ -5,40 +5,41 @@ struct neuron{
 	double out, error;
 };
 
+
 int main()
 {
 	//Инициализация слоев нейронной сети
-	neuron *n0 = new neuron[17]; //Нейроны входного слоя. 16 под пиксели + нейрон смещения //временно черно-белые
-	n0[16].out = 1;
-	n0[16].error = 0;
-	neuron *n1 = new neuron[10];
-	n1[9].out = 1;
-	n1[9].error = 0;
-	neuron *n2 = new neuron[7];
-	n2[6].out = 1;
-	n2[6].error = 0;
+	neuron *neuro0 = new neuron[17]; //Нейроны входного слоя. 16 под пиксели + нейрон смещения //временно черно-белые
+	neuro0[16].out = 1;
+	neuro0[16].error = 0;
+	neuron *neuro1 = new neuron[10];
+	neuro1[9].out = 1;
+	neuro1[9].error = 0;
+	neuron *neuro2 = new neuron[7];
+	neuro2[6].out = 1;
+	neuro2[6].error = 0;
 	neuron *n3 = new neuron[4]; //Нейроны выходного слоя по одному под каждое направление линии
 
 	//Инициализация матриц связи между слоями
-	double **w01 = new double*[17];
+	double **weight01 = new double*[17];
 	for (int i = 0; i < 17; i++) {
-		w01[i] = new double[9]; //Количество столбцов на 1 меньше размера след. массива из-за нейрона смещения
+		weight01[i] = new double[9]; //Количество столбцов на 1 меньше размера след. массива из-за нейрона смещения
 		for (int j = 0; j < 9; j++) {
-			w01[i][j] = 1; //Изначально все веса заполняются еденицами
+			weight01[i][j] = 1; //Изначально все веса заполняются еденицами
 		}
 	}
-	double **w12 = new double*[10];
+	double **weight12 = new double*[10];
 	for (int i = 0; i < 10; i++) {
-		w12[i] = new double[6];
+		weight12[i] = new double[6];
 		for (int j = 0; j < 6; j++) {
-			w12[i][j] = 1;
+			weight12[i][j] = 1;
 		}
 	}
-	double **w23 = new double*[7];
+	double **weight23 = new double*[7];
 	for (int i = 0; i < 7; i++) {
-		w23[i] = new double[4];
+		weight23[i] = new double[4];
 		for (int j = 0; j < 4; j++) {
-			w23[i][j] = 1;
+			weight23[i][j] = 1;
 		}
 	}
 }
