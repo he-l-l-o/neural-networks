86e84dd09f090461a5ed9d0604737b3a176f0fe0
MadMarchello228 <mark1010_00@mail.ru>
2019-04-29 01:30:46 +0700

    Creating a menu texture

--- neyron_base_data_creator/neyron_base_data_creator/main.cpp
+++ neyron_base_data_creator/neyron_base_data_creator/main.cpp
@@ -8,6 +8,16 @@ int main()
 {
 	//создание главного окна приложения
 	RenderWindow window(VideoMode(1200, 720), "NeyronBaseDataCreator");
+	//графика основного интерфейса
+	Image main_image;
+	main_image.loadFromFile("img/menu.png");
+
+	Texture main_texture;
+	main_texture.loadFromImage(main_image);
+
+	Sprite main_sprite;
+	main_sprite.setTexture(main_texture);
+	main_sprite.setPosition(0, 0);
 
 	while (window.isOpen())
 	{
