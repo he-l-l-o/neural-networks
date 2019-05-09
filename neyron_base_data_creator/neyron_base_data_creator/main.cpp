13447005f632b05bd76038e7fbd4ffa4ba219ba6
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-05-10 02:02:46 +0700

    Add CI AppVeyor.yml

--- neyron_base_data_creator/neyron_base_data_creator/main.cpp
+++ neyron_base_data_creator/neyron_base_data_creator/main.cpp
@@ -4,14 +4,24 @@
 
 using namespace std;
 using namespace sf;
-
+/*
+	[x][y][1] = R;
+	[x][y][2] = G;
+	[x][y][3] = B;
+	 
+	x = size_of_x;
+	y = size_of_y;
+*/
 int main()
 {
-	string file_location;
-	getline(cin, file_location);
+	short ***color_of_image_pixels;
+
+	string file_location = "D:\\load.jpg";
+	//getline(cin, file_location);
 	//создание главного окна приложени€
-	RenderWindow window(VideoMode(1200, 720), "NeyronBaseDataCreator");
 	//графика основного интерфейса
+	RenderWindow window(VideoMode(1200, 720), "NeyronBaseDataCreator");
+
 	Image main_image;
 	main_image.loadFromFile("img/menu.png");
 
@@ -31,17 +41,11 @@ int main()
 	Sprite load_image_sprite;
 	load_image_sprite.setTexture(load_image_texture);
 	load_image_sprite.setPosition(22, 20);
-
+	
 	Sprite save_image_sprite;
-	//text string
-	//объ€вление шрифта
-	//Font font;
-	//font.loadFromFile("9041.ttf");
-	//объ€вление окна-разметчика
-	RectangleShape rectangle_razmer(Vector2f(8,8));
+
+	RectangleShape rectangle_razmer(Vector2f(8, 8));
 	rectangle_razmer.setFillColor(Color::Red);
-	rectangle_razmer.setOutlineThickness(10);
-	rectangle_razmer.setOutlineColor(Color(250, 150, 100));
 	rectangle_razmer.setPosition(22, 20);
 	//new .cpp and .h
 	/////////////////////////////////////
@@ -50,11 +54,10 @@ int main()
 	cout << "x = " << size_of_x << " y = " << size_of_y << endl;
 
 	double relationship_x_y = (double)size_of_x / size_of_y;
-	cout << "Relationshio x -> y: " << relationship_x_y;
-
+	cout << "Relationship x -> y: " << relationship_x_y;
+	//масштабирование относительно изображени€ относительно окна
 	int save_size_of_x = size_of_x;
 	int save_size_of_y = size_of_y;
-
 	if (size_of_x > size_of_y) {
 		while (size_of_x > 608) {
 			size_of_x = size_of_x - 1;
@@ -70,21 +73,23 @@ int main()
 		}
 	}
 	cout << endl;
-	cout << size_of_x << " " << size_of_y << endl;
-	cout << save_size_of_x << " " << save_size_of_y;
+	cout << "Now size X " << size_of_x << " Now size Y " << size_of_y << endl;
+	cout << "Save size X " << save_size_of_x << " Save size Y " << save_size_of_y;
 	cout << endl;
 	///////////////////////////////////////////////
 	Vector2f targetSize(608, 600);
 	load_image_sprite.setScale(
 		targetSize.x / load_image_sprite.getGlobalBounds().width,
 		targetSize.y / load_image_sprite.getGlobalBounds().height);
-	
+
+	Vector2f target_Size_save(198, 198);
+
 	int location_marker_x;
 	int location_marker_y;
 
 	location_marker_x = rectangle_razmer.getPosition().x;
 	location_marker_y = rectangle_razmer.getPosition().y;
-	
+	cout << "loaction x = " << location_marker_x << " location y = " << location_marker_y;
 	RectangleShape rect_save;
 
 	while (window.isOpen())
@@ -92,16 +97,14 @@ int main()
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == Event::Closed) {
 				window.close();
-			if (event.KeyPressed)
-			{
-				if ((event.key.code == Keyboard::Right) || (event.key.code == Keyboard::Left))
-				{
+			}
+			if (event.KeyPressed) {
+				if ((event.key.code == Keyboard::Right) || (event.key.code == Keyboard::Left)) {
 					location_marker_x = rectangle_razmer.getPosition().x;
 				}
-				if ((event.key.code == Keyboard::Up) || (event.key.code == Keyboard::Down))
-				{
+				if ((event.key.code == Keyboard::Up) || (event.key.code == Keyboard::Down)) {
 					location_marker_y = rectangle_razmer.getPosition().y;
 				}
 			}
@@ -119,11 +122,14 @@ int main()
 					//form.top -= 20;
 					save_image_sprite.setTexture(load_image_texture);
 					save_image_sprite.setTextureRect(form);
-					save_image_sprite.setPosition(600, 300);
+					save_image_sprite.setScale(
+						target_Size_save.x / save_image_sprite.getGlobalBounds().width,
+						target_Size_save.y / save_image_sprite.getGlobalBounds().height);
+					save_image_sprite.setPosition(649, 21);
 				}
 			}
 		}
-		
+
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			rectangle_razmer.move(-1, 0);
 		}
@@ -136,7 +142,7 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			rectangle_razmer.move(0, 1);
 		}
-		
+
 		///////new cppp
 
 		window.clear();
@@ -146,6 +152,4 @@ int main()
 		window.draw(save_image_sprite);
 		window.display();
 	}
-
-	return 0;
 }
