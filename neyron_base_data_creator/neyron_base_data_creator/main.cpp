6e9d1c07838ee39b9477633678944c1643bf0bd2
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-06-05 23:00:37 +0700

    Change button right diagonal lien

--- neyron_base_data_creator/neyron_base_data_creator/main.cpp
+++ neyron_base_data_creator/neyron_base_data_creator/main.cpp
@@ -207,7 +207,17 @@ int main()
 				button_type_3_sprite.setColor(Color(255, 255, 255, 255));
 				check_button_left_diagonal = 0;
 			}
-		
+			///////////////////////
+			if ((event.mouseButton.button == Mouse::Button::Left) && (event.type == Event::MouseButtonPressed) && (IntRect(892, 284, 212, 81).contains(Mouse::getPosition(window))))
+			{
+				button_type_4_sprite.setColor(Color(180, 180, 180, 255));
+				check_button_right_diagonal = 1;
+			}
+			if ((event.mouseButton.button == Mouse::Button::Left) && (event.type == Event::MouseButtonReleased) && (check_button_right_diagonal == 1))
+			{
+				button_type_4_sprite.setColor(Color(255, 255, 255, 255));
+				check_button_right_diagonal = 0;
+			}
 			//////////////////////
 			if ((event.KeyPressed) && (event.key.code == Keyboard::Right) || (event.key.code == Keyboard::Left))
 			{
