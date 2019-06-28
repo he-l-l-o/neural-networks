023d2ab94b799b7f9a952979b4fbd6426bbf0158
MadMarchello228 <mark1010_00@mail.ru>
2019-06-28 16:29:28 +0700

    Init textbox class

---
+++ neyron_base_data_creator/neyron_base_data_creator/textfield.cpp
@@ -0,0 +1,9 @@
+#include "text_field.h"
+#include <string>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+TextField::TextField() {
+
+
+}
---
+++ neyron_base_data_creator/neyron_base_data_creator/textfield.h
@@ -0,0 +1,14 @@
+#ifndef TXTFIELD_H
+#define TXTFIELD_H
+
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class TextField {
+public:
+	TextField();
+private:
+
+};
+
+#endif
