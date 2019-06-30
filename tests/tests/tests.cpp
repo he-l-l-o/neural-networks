6b90a2a5a03bab29df40991209a546d8db1e08f1
Артём Гребцов <he-l-l-o@mail.ru>
2019-06-30 17:33:40 +0700

    fix formatting

--- tests/tests/tests.cpp
+++ tests/tests/tests.cpp
@@ -2,13 +2,13 @@
 #include "catch.hpp"
 #include "..\\..\\algorhytm\algorhytm\neuro.h"
 
-
-TEST_CASE("Basic neural network testing") {
-	neuron *neuro0 = new neuron[1];
-	neuron *neuro1 = new neuron[1];
+TEST_CASE("Basic neural network testing")
+{
+	neuron* neuro0 = new neuron[1];
+	neuron* neuro1 = new neuron[1];
 	neuro0[0].out = 1;
 	neuro1[0].out = 0;
-	double** weight = new double*[1];
+	double** weight = new double* [1];
 	weight[0] = new double;
 	weight[0][0] = 1;
 	forward(neuro0, 1, neuro1, 2, weight);
@@ -29,7 +29,6 @@ TEST_CASE("Basic neural network testing") {
 	REQUIRE(neuro0[0].error >= -0.15);
 	REQUIRE(neuro0[0].error <= -0.05);
 
-
 	neuro1[0].error = 1;
 	neuro0[0].out = 1;
 	weights_update(neuro1, 2, neuro0, 1, weight, 1);
@@ -37,7 +36,8 @@ TEST_CASE("Basic neural network testing") {
 	REQUIRE(weight[0][0] <= 2.05);
 }
 
-TEST_CASE("Side functions") {
+TEST_CASE("Side functions")
+{
 	int size = 3;
 	double* arr = new double[size];
 
