1fe9d3262bb5c89d3a7c8d8750f075323934bbb1
MadMarchello228 <mark1010_00@mail.ru>
2019-06-30 20:38:32 +0700

    Sprite color change at the touch of a button

--- neyron_base_data_creator/neyron_base_data_creator/image_conversion.cpp
+++ neyron_base_data_creator/neyron_base_data_creator/image_conversion.cpp
@@ -20,13 +20,24 @@ void scaling(int& size_x, int& size_y, int save_size_x, int save_size_y, double 
 		}
 	}
 }
-
-void get_color_array(short color_of_image_pixels[4][4], sf::Image load_image, sf::Event event, int location_marker_x, int location_marker_y) {
+//0 - red, 1 - green, 2 - blue
+//переделать 
+void get_color_array(short color_of_image_pixels[4][4], sf::Image load_image, sf::Event event, int location_marker_x, int location_marker_y, int type_color) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).r;
-			color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).g;
-			color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).b;
+
+			switch (type_color) {
+			case 0:
+				color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).r;
+				break;
+			case 1:
+				color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).g;
+				break;
+			case 2:
+				color_of_image_pixels[j][i] = load_image.getPixel(location_marker_x + j, location_marker_y + i).b;
+				break;
+			default: exit(4);
+			}
 		}
 	}
 }
@@ -41,7 +52,7 @@ int check_load_img(sf::Image load_image, std::string & file_location) {
 	}
 }
 
-void increase(sf::Texture& load_image_texture, sf::Sprite& save_image_sprite, int location_marker_y, int location_marker_x, sf::Vector2f target_Size_save) {
+void increase(sf::Texture & load_image_texture, sf::Sprite & save_image_sprite, int location_marker_y, int location_marker_x, sf::Vector2f target_Size_save, int type_button_color) {
 	sf::IntRect form;
 	form.height = 4;
 	form.width = 4;
@@ -55,4 +66,18 @@ void increase(sf::Texture& load_image_texture, sf::Sprite& save_image_sprite, in
 	save_image_sprite.setScale(
 		target_Size_save.x / 4,
 		target_Size_save.y / 4);
+
+	switch (type_button_color) {
+	case 0: 
+		save_image_sprite.setColor(sf::Color(255, 0, 0)); 
+		break;
+	case 1: 
+		save_image_sprite.setColor(sf::Color(0, 255, 0)); 
+		break;
+	case 2: 
+		save_image_sprite.setColor(sf::Color(0, 0, 255));
+		break;
+	default: exit(4);
+	}
+
 }
